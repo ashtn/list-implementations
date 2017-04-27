2c9e92f6247057c59bd48b4f3b105b82ccdcd03b
Ashtn <ashtonehorton@gmail.com>
2017-04-27 11:32:49 -0700

    last week stuff

--- lib/array_list.rb
+++ lib/array_list.rb
@@ -2,26 +2,56 @@
 
 class ArrayList
   def initialize
-    @storage = []
+    @storage = [0,0,0,0,0,0,0,0,0,0] #native array: fixed size is the fixed capacity, indexing
+    @size = 0
   end
 
   # Adds _value_ at the end of the list
   def add(value)
+    @storage[@size] = value
+    @size += 1
   end
 
   # Deletes the _last_ value in the array
   def delete
+    raise "error" if @size == 0
+    # @storage[@size] # not neccessary
+    @size -= 1
   end
 
   def include?(key)
+    @size.times do |i|
+      return true if @storage[i] == key
+    end
+    return false
   end
 
   def size
+    return @size
   end
 
   def max
+    raise "error" if @size == 0
+    largest = @storage[0] #don't return a weird place holder number
+    @size.times do |i|
+      if @storage[i] > largest
+        largest = @storage[i]
+      end
+    end
+    return max
   end
 
-  def to_s
+  def remove_all(value)
+    @size.times do | i |
+      # TODO BRB
+    end
+  end
+
+  def to_s #toString
+    str = ""
+    @size.times do |i|
+      str += "#{@storage[i]}" + ", "
+    end
+    return "[#{str[0..-3]}]"
   end
 end
